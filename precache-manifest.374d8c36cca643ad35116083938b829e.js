8c9056201b5e0726efc5ae8d14365705e9a64977
Tomasz <40639418+TiPiKej@users.noreply.github.com>
2020-12-07 02:25:19 +0100

    Updates

---
+++ precache-manifest.374d8c36cca643ad35116083938b829e.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = [
+  {
+    "revision": "395312b7572dc7bb0a28",
+    "url": "/PortfolioCwiczenia/css/JiggleButton.a09f9298.css"
+  },
+  {
+    "revision": "395312b7572dc7bb0a28",
+    "url": "/PortfolioCwiczenia/js/JiggleButton.59df7985.js"
+  },
+  {
+    "revision": "2e0ad14e73d1452b6117",
+    "url": "/PortfolioCwiczenia/css/app.7f9ce0ca.css"
+  },
+  {
+    "revision": "2e0ad14e73d1452b6117",
+    "url": "/PortfolioCwiczenia/js/app.005f5a62.js"
+  },
+  {
+    "revision": "c27b6c04321bdafce3f8",
+    "url": "/PortfolioCwiczenia/css/calc.c684bbfa.css"
+  },
+  {
+    "revision": "c27b6c04321bdafce3f8",
+    "url": "/PortfolioCwiczenia/js/calc.c8df9af8.js"
+  },
+  {
+    "revision": "af46a7ae7f444e02d865",
+    "url": "/PortfolioCwiczenia/css/chunk-319d.d494a89d.css"
+  },
+  {
+    "revision": "af46a7ae7f444e02d865",
+    "url": "/PortfolioCwiczenia/js/chunk-319d.ad53b540.js"
+  },
+  {
+    "revision": "ba98e3d9af504890d213",
+    "url": "/PortfolioCwiczenia/css/chunk-9f8e.1400ba61.css"
+  },
+  {
+    "revision": "ba98e3d9af504890d213",
+    "url": "/PortfolioCwiczenia/js/chunk-9f8e.369a6702.js"
+  },
+  {
+    "revision": "f1b0b918381580513c55",
+    "url": "/PortfolioCwiczenia/js/chunk-vendors.7d39e0f1.js"
+  },
+  {
+    "revision": "6b10d22b662a8bf6efc6",
+    "url": "/PortfolioCwiczenia/css/loaders.df3c2db3.css"
+  },
+  {
+    "revision": "6b10d22b662a8bf6efc6",
+    "url": "/PortfolioCwiczenia/js/loaders.6b32aa29.js"
+  },
+  {
+    "revision": "26fb77434ed7426d24a5",
+    "url": "/PortfolioCwiczenia/css/settings.c767aadd.css"
+  },
+  {
+    "revision": "26fb77434ed7426d24a5",
+    "url": "/PortfolioCwiczenia/js/settings.eb3581fd.js"
+  },
+  {
+    "revision": "e3847cc0c41d3c91fdd000170aa1dce4",
+    "url": "/PortfolioCwiczenia/index.html"
+  },
+  {
+    "revision": "7ab7df5e978b479aae6aa09da6a2a219",
+    "url": "/PortfolioCwiczenia/img/code-coding-computer-270373.jpg"
+  },
+  {
+    "revision": "7769e097e1724bba4f897a092a181a2a",
+    "url": "/PortfolioCwiczenia/img/my.jpg"
+  },
+  {
+    "revision": "aa8fb7cd4e62e6aa9f7b30d11c7dc6d9",
+    "url": "/PortfolioCwiczenia/img/p/1.jpg"
+  },
+  {
+    "revision": "556b6d16594697022fe61cdc3ed3d31d",
+    "url": "/PortfolioCwiczenia/img/codes-coding-computer-247791.jpg"
+  },
+  {
+    "revision": "c2d24d81fd5cb7475ee3b6a29a7b650b",
+    "url": "/PortfolioCwiczenia/img/p/2.jpg"
+  },
+  {
+    "revision": "a51cff14f8882782f6b82b7755435764",
+    "url": "/PortfolioCwiczenia/img/p/3.jpg"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/PortfolioCwiczenia/robots.txt"
+  },
+  {
+    "revision": "08e7985e6cacb85ccba221886f5ef368",
+    "url": "/PortfolioCwiczenia/img/z.jpg"
+  },
+  {
+    "revision": "21ca144f202d3bf00e6f8500e04af0ab",
+    "url": "/PortfolioCwiczenia/img/p/1.png"
+  }
+];
